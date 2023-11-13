17f2e9e8c6280459671a4c6b788c55e3aea0327b
ahmad <aalmadani111@gmail.com>
2023-11-13 18:30:52 +0100

    add view + list.html + bootstrap design

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path , include
 from .api import ToDoViewset
+from .views import todo_list
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('',ToDoViewset)
 urlpatterns = [
+    path('' , todo_list),
     path('api/' , include(router.urls)),
+    
 ]
